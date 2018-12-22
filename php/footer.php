549c41aa358fb6cacbda5f926fc29f76713a77f3
Cyterzen <44494198+Cyterzen@users.noreply.github.com>
2018-12-22 14:12:12 +0300

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,6 @@
+<link href="styles.css" rel="stylesheet" type="text/css"/>
+<div class="footer">
+  hotelmog@gmail.com | 8(029)1234567
+   <a href="http://facebook.com" target="_blank"> <img  src="images/facebook.png" width="20" height="20" style="right:50;"></a> <a href="http://youtube.com" target="_blank"> <img  src="images/youtube.png" width="20" height="20"style="right:50;" ></a>
+</div>
+     </body>
---
+++ php/header.php
@@ -0,0 +1,19 @@
+<! DOCTYPE html>
+<?php require "db.php";?>
+    <head>
+	    <meta charset="UTF-8"/>
+	    <link href="styles.css" rel="stylesheet" type="text/css"/>
+		<title>Mogilev Spa Resort Hotel</title>
+		<link rel="icon" href="images/logo1.png">
+		</head>
+	
+	<body> 
+	
+	<div class="topnav">
+      <a href="index.php">HOME</a>
+	  <a href="rooms.php">ROOMS</a>
+      <a href="about.php">ABOUT</a>
+	  <a href="gallery.php">GALLERY</a>
+      <a href="reservation.php">RESERVATION</a>
+	  <a href="contact.php">CONTACT</a>
+     </div>
